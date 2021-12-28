bd04f20a77dc50ca752a39cbf2a6047116907b3b
exejar <themmludwig@gmail.com>
2021-12-28 03:49:40 -0500

    Added ClearHandler for clear weather

---
+++ src/main/java/dev/salmon/weatherchanger/handler/ClearHandler.java
@@ -0,0 +1,12 @@
+package dev.salmon.weatherchanger.handler;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraftforge.client.IRenderHandler;
+
+public class ClearHandler extends IRenderHandler {
+    @Override
+    public void render(float partialTicks, WorldClient world, Minecraft mc) {
+
+    }
+}
