be9503d8e93e477b256738b80418ca33787df63a
exejar <themmludwig@gmail.com>
2021-12-28 02:37:31 -0500

    Replaced entire Essential dependency with Vigilance. Added methods to register Forge commands and event listeners. Changed WeatherChangerCommand to work as a Forge command.

--- src/main/java/dev/salmon/weatherchanger/WeatherChanger.java
+++ src/main/java/dev/salmon/weatherchanger/WeatherChanger.java
@@ -2,10 +2,14 @@ package dev.salmon.weatherchanger;
 
 import dev.salmon.weatherchanger.command.WeatherChangerCommand;
 import dev.salmon.weatherchanger.config.WeatherConfig;
+import net.minecraft.command.ICommand;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import java.util.Arrays;
 
 @Mod(modid = WeatherChanger.ID, name = WeatherChanger.NAME, version = WeatherChanger.VER)
 public class WeatherChanger {
@@ -20,11 +24,30 @@ public class WeatherChanger {
 
     @Mod.EventHandler
     protected void init(FMLInitializationEvent event) {
-        new WeatherChangerCommand().register();
         this.config = new WeatherConfig();
+        this.registerCommands(new WeatherChangerCommand());
     }
 
     public WeatherConfig getConfig() { return this.config; }
+
+    /* Used to register forge event listeners */
+    public static void registerListeners(Object... objects) {
+        for (Object o : objects) {
+            MinecraftForge.EVENT_BUS.register(o);
+        }
+    }
+
+    /* Used to unregister forge event listeners */
+    public static void unregisterListeners(Object... objects) {
+        for (Object o : objects) {
+            MinecraftForge.EVENT_BUS.unregister(o);
+        }
+    }
+
+    /* Used to register Forge commands. Should only be used on startup */
+    private void registerCommands(ICommand... command) {
+        Arrays.stream(command).forEachOrdered(ClientCommandHandler.instance::registerCommand);
+    }
 
     public static WeatherChanger getWeatherChanger() { return instance; }
 }
