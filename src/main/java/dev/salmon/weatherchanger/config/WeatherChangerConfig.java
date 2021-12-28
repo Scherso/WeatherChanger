e982f5ee7c0cea406f77807a27c17475ff025d00
Scherso <samsporidis@gmail.com>
2021-12-28 00:34:05 -0500

    Remove mixin gradle, as it is not needed. Gradle cleanup

--- src/main/java/dev/salmon/weatherchanger/config/WeatherChangerConfig.java
+++ src/main/java/dev/salmon/weatherchanger/config/WeatherChangerConfig.java
@@ -12,6 +12,28 @@ import java.io.File;
 
 public class WeatherChangerConfig extends Vigilant {
     @Property(
+            type = PropertyType.SELECTOR,
+            name = "Pick the Weather",
+            description = "Control the Weather!",
+            category = "General",
+            options = {"Clear", "Snow", "Rain"}
+    )
+    public static int currentWeather = 0;
+    // 0 == Clear, 1 == Snow, 2 == Rain
+
+    @Property(
+            type = PropertyType.DECIMAL_SLIDER,
+            name = "Intensity",
+            description = "Allows you to control the intensity or opacity of the weather Particles.",
+            category = "General",
+            minF = 0f,
+            maxF = 1.0f
+    )
+    public static float strength = 1f;
+    // We'll get this working later
+
+    // don't worry about what's down here
+    @Property(
             type = PropertyType.SWITCH,
             name = "Show Update Notification",
             description = "Show a notification when you start Minecraft informing you of new updates.",
@@ -33,7 +55,7 @@ public class WeatherChangerConfig extends Vigilant {
     }
 
     public WeatherChangerConfig() {
-        super(new File(WeatherChanger.modDir, WeatherChanger.ID + ".toml"), WeatherChanger.NAME);
+        super(new File(WeatherChanger.modDir, WeatherChanger.ID + ".toml"), "Weather Changer");
         initialize();
     }
 }
