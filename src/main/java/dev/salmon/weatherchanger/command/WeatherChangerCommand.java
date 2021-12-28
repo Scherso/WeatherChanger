5290dae0bbeda022b894b323b021fee8e9b89054
exejar <themmludwig@gmail.com>
2021-12-28 02:37:31 -0500

    Replaced entire Essential dependency with Vigilance. Added methods to register Forge commands and event listeners. Changed WeatherChangerCommand to work as a Forge command.

--- src/main/java/dev/salmon/weatherchanger/command/WeatherChangerCommand.java
+++ src/main/java/dev/salmon/weatherchanger/command/WeatherChangerCommand.java
@@ -1,17 +1,40 @@
 package dev.salmon.weatherchanger.command;
 
 import dev.salmon.weatherchanger.WeatherChanger;
-import gg.essential.api.EssentialAPI;
-import gg.essential.api.commands.Command;
-import gg.essential.api.commands.DefaultHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-public class WeatherChangerCommand extends Command {
-    public WeatherChangerCommand() {
-        super(WeatherChanger.ID, true);
+public class WeatherChangerCommand extends CommandBase {
+
+    @Override
+    public String getCommandName() {
+        return WeatherChanger.ID;
     }
 
-    @DefaultHandler
-    public void handle() {
-        EssentialAPI.getGuiUtil().openScreen(WeatherChanger.getWeatherChanger().getConfig().gui());
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return "Opens GUI for WeatherChanger";
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(ICommandSender sender) {
+        return true;
+    }
+
+    /* Minecraft will close the GuiScreen instantly if you do not display it 1 tick after the command is sent */
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        WeatherChanger.registerListeners(this);
+    }
+
+    @SubscribeEvent
+    public void openGui(TickEvent.ClientTickEvent event) {
+        Minecraft.getMinecraft().displayGuiScreen(WeatherChanger.getWeatherChanger().getConfig().gui());
+        WeatherChanger.unregisterListeners(this);
     }
 }
