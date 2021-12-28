a0c16fbb2106f78229eb73a8a08f8ebd3c3bbbc1
exejar <themmludwig@gmail.com>
2021-12-28 01:22:36 -0500

    Removed all static abuse, and removed janky file updater.

--- src/main/java/dev/salmon/weatherchanger/WeatherChanger.java
+++ src/main/java/dev/salmon/weatherchanger/WeatherChanger.java
@@ -2,32 +2,29 @@ package dev.salmon.weatherchanger;
 
 import dev.salmon.weatherchanger.command.WeatherChangerCommand;
 import dev.salmon.weatherchanger.config.WeatherChangerConfig;
-import dev.salmon.weatherchanger.updater.Updater;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-import java.io.File;
 
 @Mod(modid = WeatherChanger.ID, name = WeatherChanger.NAME, version = WeatherChanger.VER)
 public class WeatherChanger {
     public static final String NAME = "@NAME@", VER = "@VER@", ID = "@ID@";
-    public static File jarFile;
-    public static File modDir = new File(new File(Minecraft.getMinecraft().mcDataDir, "W-OVERFLOW"), NAME);
-    public static WeatherChangerConfig config;
+    private static WeatherChanger instance;
+    private WeatherChangerConfig config;
 
     @Mod.EventHandler
-    protected void onFMLPreInitialization(FMLPreInitializationEvent event) {
-        if (!modDir.exists()) modDir.mkdirs();
-        jarFile = event.getSourceFile();
+    protected void preInit(FMLPreInitializationEvent event) {
+        instance = this;
     }
 
     @Mod.EventHandler
-    protected void onInitialization(FMLInitializationEvent event) {
+    protected void init(FMLInitializationEvent event) {
         new WeatherChangerCommand().register();
-        config = new WeatherChangerConfig();
-        config.preload();
-        Updater.update();
+        this.config = new WeatherChangerConfig();
     }
+
+    public WeatherChangerConfig getConfig() { return this.config; }
+
+    public static WeatherChanger getWeatherChanger() { return instance; }
 }
--- src/main/java/dev/salmon/weatherchanger/config/WeatherChangerConfig.java
+++ src/main/java/dev/salmon/weatherchanger/config/WeatherChangerConfig.java
@@ -1,9 +1,6 @@
 package dev.salmon.weatherchanger.config;
 
 import dev.salmon.weatherchanger.WeatherChanger;
-import dev.salmon.weatherchanger.updater.DownloadGui;
-import dev.salmon.weatherchanger.updater.Updater;
-import gg.essential.api.EssentialAPI;
 import gg.essential.vigilance.Vigilant;
 import gg.essential.vigilance.data.Property;
 import gg.essential.vigilance.data.PropertyType;
@@ -11,29 +8,19 @@ import gg.essential.vigilance.data.PropertyType;
 import java.io.File;
 
 public class WeatherChangerConfig extends Vigilant {
+    public WeatherChangerConfig() {
+        super(new File("./config", WeatherChanger.ID + ".toml"), WeatherChanger.NAME);
+        this.initialize();
+        this.preload();
+    }
+
     @Property(
             type = PropertyType.SWITCH,
             name = "Show Update Notification",
             description = "Show a notification when you start Minecraft informing you of new updates.",
             category = "Updater"
     )
-    public static boolean showUpdate = true;
+    private boolean showUpdate = true;
 
-    @Property(
-            type = PropertyType.BUTTON,
-            name = "Update Now",
-            description = "Update by clicking the button.",
-            category = "Updater"
-    )
-    public void update() {
-        if (Updater.shouldUpdate) EssentialAPI.getGuiUtil()
-                .openScreen(new DownloadGui());
-        else EssentialAPI.getNotifications()
-                .push(WeatherChanger.NAME, "No update had been detected at startup, and thus the update GUI has not been shown.");
-    }
-
-    public WeatherChangerConfig() {
-        super(new File(WeatherChanger.modDir, WeatherChanger.ID + ".toml"), WeatherChanger.NAME);
-        initialize();
-    }
+    public boolean isShowUpdate() { return this.showUpdate; }
 }
