f19e819503d53f44632246ca8fdd26ca6e9501e2
exejar <themmludwig@gmail.com>
2021-12-28 16:17:21 -0500

    Added check to remove world weather while using custom weather handlers

--- src/main/java/dev/salmon/weatherchanger/listener/WeatherListener.java
+++ src/main/java/dev/salmon/weatherchanger/listener/WeatherListener.java
@@ -2,11 +2,18 @@ package dev.salmon.weatherchanger.listener;
 
 import dev.salmon.weatherchanger.WeatherChanger;
 import dev.salmon.weatherchanger.handler.*;
+import dev.salmon.weatherchanger.handler.weather.ClearHandler;
+import dev.salmon.weatherchanger.handler.weather.RainHandler;
+import dev.salmon.weatherchanger.handler.weather.SnowHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.world.storage.WorldInfo;
 import net.minecraftforge.client.IRenderHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.Random;
 
 public class WeatherListener {
     @SubscribeEvent
@@ -18,16 +25,28 @@ public class WeatherListener {
 
             if (currentWeather == 0 && currentWeatherHandler instanceof WeatherHandler) {
                 world.provider.setWeatherRenderer(null);
+                /* Reset worldInfo cleanWeather timer */
+                int i = (300 + (new Random()).nextInt(600)) * 20;
+                Minecraft.getMinecraft().theWorld.getWorldInfo().setCleanWeatherTime(i);
             } else if (currentWeather == 1 && !(currentWeatherHandler instanceof ClearHandler)) {
                 world.provider.setWeatherRenderer(new ClearHandler());
             } else if (currentWeather == 2 && !(currentWeatherHandler instanceof SnowHandler)) {
                 world.provider.setWeatherRenderer(new SnowHandler());
             } else if (currentWeather == 3 && !(currentWeatherHandler instanceof RainHandler)) {
-                world.provider.setWeatherRenderer(new RainHandler());
+                world.provider.setWeatherRenderer(new RainHandler(false));
             }
 
             if (currentWeatherHandler instanceof WeatherHandler && !Minecraft.getMinecraft().isGamePaused()) {
                 ((WeatherHandler) currentWeatherHandler).update();
+
+                WorldInfo worldInfo = Minecraft.getMinecraft().theWorld.getWorldInfo();
+                if (worldInfo.isRaining()) {
+                    worldInfo.setCleanWeatherTime(1000000000);
+                    worldInfo.setRainTime(0);
+                    worldInfo.setThunderTime(0);
+                    worldInfo.setRaining(false);
+                    worldInfo.setThundering(false);
+                }
             }
         }
     }
