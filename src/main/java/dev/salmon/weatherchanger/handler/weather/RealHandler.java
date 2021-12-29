eaca79857d7ecf3aa5f39e3dafee1c918dd4f5a9
exejar <themmludwig@gmail.com>
2021-12-28 20:40:15 -0500

    Added enum to help handle the weather response

--- src/main/java/dev/salmon/weatherchanger/handler/weather/RealHandler.java
+++ src/main/java/dev/salmon/weatherchanger/handler/weather/RealHandler.java
@@ -24,11 +24,10 @@ import java.nio.charset.StandardCharsets;
 public class RealHandler extends WeatherHandler {
     private long lastWeatherCheck;
     private final JsonParser jsonParser = new JsonParser();
+    private WeatherHandler weatherRender;
 
     @Override
     public void update() {
-        this.rendererUpdateCount++;
-
         /* Checks local weather every minute */
         if (System.currentTimeMillis() - this.lastWeatherCheck > 60 * 1000) {
             this.lastWeatherCheck = System.currentTimeMillis();
@@ -50,8 +49,6 @@ public class RealHandler extends WeatherHandler {
 
                 String url = String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", location, apiKey);
 
-                System.out.println(url);
-
                 JsonObject response = new JsonObject();
                 try (CloseableHttpClient client = HttpClients.createDefault()) {
                     HttpGet request = new HttpGet(url);
@@ -67,16 +64,112 @@ public class RealHandler extends WeatherHandler {
                         return;
                     }
 
-                    System.out.println(response);
+                    int weatherId = response.get("weather").getAsJsonArray().get(0).getAsJsonObject().get("id").getAsInt();
+                    /* only reason this should return null is if OpenWeatherMap changes their ID's */
+                    WeatherType weatherType = WeatherType.fromId(weatherId);
+                    System.out.println(weatherType.toString());
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
             });
+
+//            if (!this.mc.isGamePaused())
+//                this.weatherRender.update();
         }
     }
 
     @Override
     public void render(float partialTicks, WorldClient world, Minecraft mc) {
+//        this.weatherRender.render(partialTicks, world, mc);
+    }
 
+    public enum WeatherType {
+        THUNDERSTORM_WITH_LIGHT_RAIN(200, "Thunderstorm"),
+        THUNDERSTORM_WITH_RAIN(201, "Thunderstorm"),
+        THUNDERSTORM_WITH_HEAVY_RAIN(202, "Thunderstorm"),
+        LIGHT_THUNDERSTORM(210, "Thunderstorm"),
+        THUNDERSTORM(211, "Thunderstorm"),
+        HEAVY_THUNDERSTORM(212, "Thunderstorm"),
+        RAGGED_THUNDERSTORM(221, "Thunderstorm"),
+        THUNDERSTORM_WITH_LIGHT_DRIZZLE(230, "Thunderstorm"),
+        THUNDERSTORM_WITH_DRIZZLE(231, "Thunderstorm"),
+        THUNDERSTORM_WITH_HEAVY_DRIZZLE(232, "Thunderstorm"),
+
+        LIGHT_INTENSITY_DRIZZLE(300, "Drizzle"),
+        DRIZZLE(301, "Drizzle"),
+        HEAVY_INTENSITY_DRIZZLE(302, "Drizzle"),
+        LIGHT_INTENSITY_DRIZZLE_RAIN(310, "Drizzle"),
+        DRIZZLE_RAIN(311, "Drizzle"),
+        HEAVY_INTENSITY_DRIZZLE_RAIN(312, "Drizze"),
+        SHOWER_RAIN_AND_DRIZZLE(313, "Drizzle"),
+        HEAVY_SHOWER_RAIN_AND_DRIZZLE(314, "Drizzle"),
+        SHOWER_DRIZZLE(321, "Drizzle"),
+
+        LIGHT_RAIN(500, "Rain"),
+        MODERATE_RAIN(501, "Rain"),
+        HEAVY_INTENSITY_RAIN(502, "Rain"),
+        VERY_HEAVY_RAIN(503, "Rain"),
+        EXTREME_RAIN(504, "Rain"),
+        FREEZING_RAIN(511, "Rain"),
+        LIGHT_INTENSITY_SHOWER_RAIN(520, "Rain"),
+        SHOWER_RAIN(521, "Rain"),
+        HEAVY_INTENSITY_SHOWER_RAIN(522, "Rain"),
+        RAGGED_SHOWER_RAIN(531, "Rain"),
+
+        LIGHT_SNOW(600, "Snow"),
+        SNOW(601, "Snow"),
+        HEAVY_SNOW(602, "Snow"),
+        SLEET(611, "Snow"),
+        LIGHT_SHOWER_SLEET(612, "Snow"),
+        SHOWER_SLEET(613, "Snow"),
+        LIGHT_RAIN_AND_SNOW(615, "Snow"),
+        RAIN_AND_SNOW(616, "Snow"),
+        LIGHT_SHOWER_SNOW(620, "Snow"),
+        SHOWER_SNOW(621, "Snow"),
+        HEAVY_SHOWER_SNOW(622, "Snow"),
+
+        MIST(701, "Mist"),
+        SMOKE(711, "Smoke"),
+        HAZE(721, "Haze"),
+        SAND_DUST_WHIRLS(731, "Dust"),
+        FOG(741, "Fog"),
+        SAND(751, "Sand"),
+        DUST(761, "Dust"),
+        VOLCANIC_ASH(762, "Ash"),
+        SQUALLS(771, "Squall"),
+        TORNADO(781, "Tornado"),
+
+        CLEAR_SKY(800, "Clear"),
+
+        FEW_CLOUDS(801, "Clouds"),
+        SCATTERED_CLOUDS(802, "Clouds"),
+        BROKEN_CLOUDS(803, "Clouds"),
+        OVERCAST_CLOUDS(804, "Clouds");
+
+        int id;
+        String category;
+
+        WeatherType(int id, String category) {
+            this.id = id;
+            this.category = category;
+        }
+
+        public static WeatherType fromId(int id) {
+            for (WeatherType type : values()) {
+                if (type.id == id)
+                    return type;
+            }
+
+            return null;
+        }
+
+        public int getId() { return this.id; }
+
+        public String getCategory() { return this.category; }
+
+        public String getName() { return this.name().replace("_", " ").toLowerCase(); }
+
+        @Override
+        public String toString() { return this.getName(); }
     }
 }
