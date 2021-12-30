3b5c55a7e251ee701080e1aa1d3ac8da36a5ae42
Deftu <deftudev@gmail.com>
2021-12-30 10:43:11 +0200

    new: Cleanup `WeatherChangerCommand` file.

--- src/main/java/dev/salmon/weatherchanger/util/Multithreading.java
+++ src/main/java/dev/salmon/weatherchanger/util/Multithreading.java
@@ -6,14 +6,14 @@ import com.google.gson.GsonBuilder;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Multithreading {
 
     private static Locale CACHED_LOCALE;
     private static Gson CACHED_GSON;
     private static final ExecutorService executorService = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("WeatherChanger-%d").build());
+    private static final ScheduledExecutorService runnableExecutor = new ScheduledThreadPoolExecutor(8);
 
     private static Locale getLocale() {
         return CACHED_LOCALE == null ? CACHED_LOCALE = new Locale(
@@ -31,6 +31,10 @@ public class Multithreading {
 
     public static void runAsync(Runnable runnable) {
         executorService.submit(runnable);
+    }
+
+    public static ScheduledFuture<?> schedule(Runnable runnable, long delay, TimeUnit timeUnit) {
+        return runnableExecutor.schedule(runnable, delay, timeUnit);
     }
 
     public static SimpleDateFormat getFormattedDate() {
