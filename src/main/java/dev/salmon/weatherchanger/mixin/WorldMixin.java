879800e16c4fa0d2b5603a3e0f355f578e64e810
Sam <90007553+Scherso@users.noreply.github.com>
2022-03-24 20:28:57 -0400

    fix: removed redundant import

--- src/main/java/dev/salmon/weatherchanger/mixin/WorldMixin.java
+++ src/main/java/dev/salmon/weatherchanger/mixin/WorldMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
